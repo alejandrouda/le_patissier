90dc3392eac7e86c5215a6e6329461212a0fff94
Alejandro Udaquiola <alejandroudaquiola1@gmail.com>
2020-08-20 12:35:38 +0200

    starting landing page design

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,78 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_081900) do
+ActiveRecord::Schema.define(version: 2020_08_20_095304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "products_id", null: false
+    t.bigint "carts_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["carts_id"], name: "index_cart_items_on_carts_id"
+    t.index ["products_id"], name: "index_cart_items_on_products_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.string "products_references"
+    t.string "orders_references"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "users_id", null: false
+    t.bigint "carts_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["carts_id"], name: "index_orders_on_carts_id"
+    t.index ["users_id"], name: "index_orders_on_users_id"
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.bigint "products_id", null: false
+    t.bigint "categories_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["categories_id"], name: "index_product_categories_on_categories_id"
+    t.index ["products_id"], name: "index_product_categories_on_products_id"
+  end
+
+  create_table "product_prices", force: :cascade do |t|
+    t.bigint "products_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["products_id"], name: "index_product_prices_on_products_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "stocks", force: :cascade do |t|
+    t.bigint "products_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["products_id"], name: "index_stocks_on_products_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +95,13 @@ ActiveRecord::Schema.define(version: 2020_08_13_081900) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cart_items", "carts", column: "carts_id"
+  add_foreign_key "cart_items", "products", column: "products_id"
+  add_foreign_key "carts", "users"
+  add_foreign_key "orders", "carts", column: "carts_id"
+  add_foreign_key "orders", "users", column: "users_id"
+  add_foreign_key "product_categories", "categories", column: "categories_id"
+  add_foreign_key "product_categories", "products", column: "products_id"
+  add_foreign_key "product_prices", "products", column: "products_id"
+  add_foreign_key "stocks", "products", column: "products_id"
 end
