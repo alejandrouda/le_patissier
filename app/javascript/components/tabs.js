dc2d7959aa500bc9ff1f17febf7f3d7391dcc293
Alejandro Udaquiola <alejandroudaquiola1@gmail.com>
2020-10-31 11:05:52 +0100

    fixing heroku bugs

--- app/javascript/components/tabs.js
+++ app/javascript/components/tabs.js
@@ -1,8 +1,7 @@
-const tabsContainer = document.querySelector('.products__tab-container');
-const productsTab = document.querySelectorAll('.products__tab');
-const tabsContent = document.querySelectorAll('.products__content');
-
 const tabs = function () {
+  const tabsContainer = document.querySelector('.products__tab-container');
+  const productsTab = document.querySelectorAll('.products__tab');
+  const tabsContent = document.querySelectorAll('.products__content');
   tabsContainer.addEventListener('click', function (event) {
     event.preventDefault();
     const clicked = event.target.closest('.products__tab');
