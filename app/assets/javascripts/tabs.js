5a69c8c29d33b51ffc79bb91f482ff8c7c239b57
Alejandro Udaquiola <alejandroudaquiola1@gmail.com>
2020-10-31 10:44:12 +0100

    working on tabs

---
+++ app/assets/javascripts/tabs.js
@@ -0,0 +1,24 @@
+const tabsContainer = document.querySelector('.products__tab-container');
+const productsTab = document.querySelectorAll('.products__tab');
+const tabsContent = document.querySelectorAll('.products__content');
+const tabActive = document.querySelector('.products__content--active');
+
+const tabs = function () {
+  tabsContainer.addEventListener('click', function (event) {
+    event.preventDefault();
+    const clicked = event.target.closest('.products__tab');
+    if (!clicked) return;
+
+    productsTab.forEach(tab => tab.classList.remove('products__tab--active'));
+    clicked.classList.add('products__tab--active');
+
+    const content = document.querySelector(
+      `.products__content--${clicked.dataset.tab}`
+    );
+    tabsContent.forEach(content =>
+      content.classList.remove('products__content--active')
+    );
+    content.classList.add('products__content--active');
+  });
+};
+export { tabs };
